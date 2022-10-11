06e1424e71a7f62cf1fbe7f890105f9b8dbe162d
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-10-11 11:12:51 -0400

    finished assignment

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public static bool IsAPalindrome(string answer)
+        {
+            if (answer == null || answer == "")
+            {
+                return false;
+            }
+            string lower = answer.ToLower();
+            string reversed = "";
+
+            for (int i = answer.Length - 1; i >=0; i--)
+            {
+                reversed += lower[i];
+            }
+
+            return lower == reversed;  
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -8,7 +9,20 @@ namespace PalindromeExerciseTests
         [Fact]
         public void Test1()
         {
-
+            [Theory]
+            [InlineData("racecar", true)]
+            [InlineData("helo", false)]
+            [InlineData("hannah", true)]
+            [InlineData("kayak", true)]
+            [InlineData("john", false)]
+            [InlineData("", false)]
+            [InlineData(null, false)]
+            void PalindromeTester(string answer, bool expected)
+            {
+                var wordsmith = new WordSmith();
+                var actual = WordSmith.IsAPalindrome(answer);
+                Assert.Equal(actual, expected); 
+            }
         }
     }
 }
